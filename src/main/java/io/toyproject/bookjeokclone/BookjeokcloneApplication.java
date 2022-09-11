60c1de31daba581a830c7bbab9699976cf629469
sua <ssuuaa.park@gmail.com>
2022-09-11 15:28:36 +0900

    first commit

---
+++ src/main/java/io/toyproject/bookjeokclone/BookjeokcloneApplication.java
@@ -0,0 +1,13 @@
+package io.toyproject.bookjeokclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookjeokcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookjeokcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/toyproject/bookjeokclone/BookjeokcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.toyproject.bookjeokclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookjeokcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
